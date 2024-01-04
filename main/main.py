1c458a212da435f6c8c4140ade25192d08bfe30e
sedrini <36553765+Sedrini@users.noreply.github.com>
2024-01-03 18:46:48 -0600

    Add features and simplify code
    1. Addition of threading to delete duplicate files.
    2. Removal of 'psg_confirm'; replaced with a popup.
    3. Addition of a checkbox to enable/disable scanning of subfolders for the selected path. Unchecked by default.
    4. Change of color in the main window.
    5. Addition of a button to cancel the scan (currently not functional).
    6. Inclusion of a popup to display the number of duplicated files and provide the option to delete them or not.
    7. Add popup when there are no duplicated files.

--- main/main.py
+++ main/main.py
@@ -1,114 +1,110 @@
 import os
-from pathlib import Path
+import threading
 import hashlib
+from pathlib import Path
 import PySimpleGUI as sg
 from send2trash import send2trash
 
-def psg_setup():
+def main():
     # Primary window used to select directory to be scanned for duplicate files
+    sg.theme('LightGreen5')
     layout = [
-        [sg.Text('Select a folder to scan')],
-        [sg.FolderBrowse(target='selected_folder')],
-        [sg.Input(key='selected_folder')],
-        [sg.Button('Scan'), sg.Button('Cancel')]
+        [sg.FolderBrowse(target='selected_folder'),sg.CB('Subfolders',key='sub_folder')],
+        [sg.Input('Select or enter a folder to scan ',key='selected_folder')],
+        [sg.Button('Scan'), sg.Button('Exit')]
     ]
-    
-    window = sg.Window('File Deduplicator', layout, grab_anywhere=True)
-    
-    return window
+
+    window = sg.Window('File Deduplicator', layout, grab_anywhere=True)    
+    while True:
+        event, values = window.read()
+        if event in (None, 'Exit'):
+            window.close()
+            break
+        if event in ('Scan'):
+            if not os.path.exists(values['selected_folder']):
+                sg.popup_error('Invalid path/folder')
+            else:
+                scan_dupe(values['selected_folder'],values['sub_folder'])
+
+    window.close()
 
 
-def psg_scan_progress(file_num):
+def scan_progress_bar(file_num):
     # Simple progress bar to monitor scan progress
     layout = [
         [sg.Text('Scanning...')],
-        [sg.ProgressBar(max_value=file_num, orientation='h', size=(20,20), key='-PBAR-')]
+        [sg.ProgressBar(max_value=file_num, orientation='h', size=(20,20), key='-PBAR-')],
+        [sg.Cancel()]#Currently it does not cancel, idk how to make it to cancel the scan
     ]
     
-    window = sg.Window('Scan Progress', layout, grab_anywhere=True)
-    
+    window = sg.Window('Scan Progress', layout, grab_anywhere=True, keep_on_top=True)   
+        
     return window
 
 
-def psg_confirm(total_files):
-    # Report how many duplicates have been found and removed
-    layout = [
-        [sg.Text(f'{total_files} files have been moved to the trash.')],
-        [sg.Button('Confirm')]
-    ]
-    
-    window = sg.Window('Duplicates Deleted', layout, grab_anywhere=True)
-    
-    return window
-
-
-def hash_file(path):
+def get_hash_file(path):
     with open(path, 'rb') as f:
         hasher = hashlib.md5()
         blocksize = 65536
         buffer = f.read(blocksize)
-        
         while len(buffer) > 0:
             hasher.update(buffer)
             buffer = f.read(blocksize)
     
-        return hasher.hexdigest()
+    return hasher.hexdigest()
 
 
-def main():
-    window = psg_setup()
-    while True:
-        event, values = window.read()
-        if event in (None, 'Cancel'):
-            print(event, values)
-            window.close()
-            break
-        
-        if event in ('Scan'):
-            # Gather the contents of our target folder
-            file_lst = os.walk(values['selected_folder'])
-            
-            # Setup values for our progress bar
-            scan_prog, total = 0, len(os.listdir(values['selected_folder']))
-            
-            # Setup our progress bar window
-            prog_window = psg_scan_progress(total)
-            progress_bar = prog_window['-PBAR-']
+def scan_dupe(folder, sub_f):        
+    # Gather the contents of our target folder
+    file_lst = os.walk(folder)
+    
+    # Setup values for our progress bar
+    scan_prog, total = 0, len(os.listdir(folder))
+    
+    # Setup our progress bar window
+    prog_window = scan_progress_bar(total)
+    progress_bar = prog_window['-PBAR-']
 
-            unique_files = {}
-            dupe_files = []
-            
-            # Loop over our target directory and resident files
-            for root, _, files in file_lst:
-                prog_window.read(timeout=0)  # Establish and update our progress bar
+    unique_files = {}; dupe_files = []
+    
+    # Loop over our target directory and resident files
+    for root, _, files in file_lst:
+        prog_window.read(timeout=0)  # Establish and update our progress bar
+        if sub_f or root == folder:
+            for file in files:
+                # Update progress bar for each file
+                scan_prog += 1
+                progress_bar.update_bar(scan_prog, total)
+                
+                # Get the full file path and hash the file accordingly
+                file_path = Path(os.path.join(root, file))            
+                file_hash = get_hash_file(file_path)
+                
+                # Add unique files to our unique_files Dict
+                # As duplicate files are encountered, add them to the dupe_file List
+                if file_hash not in unique_files:
+                    unique_files[file_hash] = file_path
+                else:
+                    dupe_files.append(file_path)
 
-                for file in files:
-                    # Update progress bar for each file
-                    scan_prog += 1
-                    progress_bar.update_bar(scan_prog, total)
-                    
-                    # Get the full file path and hash the file accordingly
-                    file_path = Path(os.path.join(root, file))
-                    file_hash = hash_file(file_path)
-                    
-                    # Add unique files to our unique_files Dict
-                    # As duplicate files are encountered, add them to the dupe_file List
-                    if file_hash not in unique_files:
-                        unique_files[file_hash] = file_path
-                    else:
-                        dupe_files.append(file_path)
-                        print(f"{file_path} appears to be a duplicate.")    
-                        send2trash(file_path)
-            
-                prog_window.close()  # Close the progress bar window
-            
-            # Report how many files have been removed
-            confirm_window = psg_confirm(len(dupe_files))
-            event_conf, _ = confirm_window.read()
-            
-            if event_conf in ('Confirm'):
-                confirm_window.close()
+    prog_window.close()  # Close the progress bar window
+
+    if len(dupe_files) > 0: 
+        yes_or_no = sg.popup_ok_cancel(f"{len(dupe_files)} files appears to be duplicated.\nWant to delete them?")
+
+        match yes_or_no:
+            case 'OK':
+                #threadingn to delete files faster 
+                thread1 = threading.Thread(target=send2trash, args=(dupe_files,))
+                thread1.start()
+                thread1.join()
+                sg.popup(f'{len(dupe_files)} files have been moved to the trash.')
+            case _:
+                sg.popup_timed('0 files were deleted.')
+    else:
+        sg.popup('There are no duplicate items in this folder')
 
 
 if __name__ == '__main__':
     main()
+    
