3ac9f47f5951aa9f1527825ca59305a6b9bd6984
sedrini <36553765+Sedrini@users.noreply.github.com>
2024-01-09 03:25:06 -0600

    Update GUI + feature extension search
    1. Updated the visual appearance and usability of the interface.
    2. Added a feature to find duplicate files with a specific extension using a Multiline field.
    3. Removed the exit button from the interface.

--- main/main.py
+++ main/main.py
@@ -1,18 +1,29 @@
 import os
 import threading
 import hashlib
-from pathlib import Path
 import PySimpleGUI as sg
+from pathlib import Path
 from send2trash import send2trash
 
 def main():
     # Primary window used to select directory to be scanned for duplicate files
+    btn_pad = ((6, 0), (10, 0))
     sg.theme('LightGreen5')
-    layout = [
-        [sg.FolderBrowse(target='selected_folder'),sg.CB('Subfolders',key='sub_folder')],
+    layout1 = [
         [sg.Input('Select or enter a folder to scan ',key='selected_folder')],
-        [sg.Button('Scan'), sg.Button('Exit')]
+        [sg.Button('Scan',pad=btn_pad),sg.FolderBrowse(target='selected_folder',pad=btn_pad)]
     ]
+
+    layout2 = [
+        [sg.CB('Subfolders',key='sub_folder')],
+        [sg.Text('File ext.')],
+        [sg.Multiline(default_text='', size=(9, 3),key='-ext-')]
+    ]
+
+    #https://stackoverflow.com/questions/71721158/how-do-i-add-elements-horizontally-instead-of-vertically-in-pysimplegui
+    layout = [
+    [sg.Column(layout1, vertical_alignment='top'), sg.VSeparator(), sg.Column(layout2, vertical_alignment='top')],
+]   
 
     window = sg.Window('File Deduplicator', layout, grab_anywhere=True)    
     while True:
@@ -20,11 +31,14 @@ def main():
         if event in (None, 'Exit'):
             window.close()
             break
-        if event in ('Scan'):
+        elif event in ('Scan'):
+            extension_custom = tuple(values['-ext-'].splitlines())
             if not os.path.exists(values['selected_folder']):
                 sg.popup_error('Invalid path/folder')
             else:
-                scan_dupe(values['selected_folder'],values['sub_folder'])
+                scan_dupe(values['selected_folder'],values['sub_folder'],extension_custom)
+                
+
 
     window.close()
 
@@ -54,13 +68,9 @@ def get_hash_file(path):
     return hasher.hexdigest()
 
 
-def scan_dupe(folder, sub_f):        
-    # Gather the contents of our target folder
-    file_lst = os.walk(folder)
-    
+def scan_dupe(directory_path, sub_f, tuple_exten):   
     # Setup values for our progress bar
-    scan_prog, total = 0, len(os.listdir(folder))
-    
+    scan_prog, total = 0, len(os.listdir(directory_path))
     # Setup our progress bar window
     prog_window = scan_progress_bar(total)
     progress_bar = prog_window['-PBAR-']
@@ -68,24 +78,26 @@ def scan_dupe(folder, sub_f):
     unique_files = {}; dupe_files = []
     
     # Loop over our target directory and resident files
-    for root, _, files in file_lst:
+    for root, dirs, files in os.walk(directory_path):
         prog_window.read(timeout=0)  # Establish and update our progress bar
-        if sub_f or root == folder:
-            for file in files:
-                # Update progress bar for each file
-                scan_prog += 1
-                progress_bar.update_bar(scan_prog, total)
-                
-                # Get the full file path and hash the file accordingly
-                file_path = Path(os.path.join(root, file))            
-                file_hash = get_hash_file(file_path)
-                
-                # Add unique files to our unique_files Dict
-                # As duplicate files are encountered, add them to the dupe_file List
-                if file_hash not in unique_files:
-                    unique_files[file_hash] = file_path
-                else:
-                    dupe_files.append(file_path)
+        if not sub_f and root != directory_path:
+            continue
+        for file in files:
+            #sort by only the file extension, if are
+            if tuple_exten and not file.endswith(tuple_exten):
+                continue  
+            # Update progress bar for each file
+            scan_prog += 1
+            progress_bar.update_bar(scan_prog, total)              
+            # Get the full file path and hash the file accordingly
+            file_path = Path(os.path.join(root, file))            
+            file_hash = get_hash_file(file_path)
+            # Add unique files to our unique_files Dict
+            # As duplicate files are encountered, add them to the dupe_file List
+            if file_hash not in unique_files:
+                unique_files[file_hash] = file_path
+            else:
+                dupe_files.append(file_path)
 
     prog_window.close()  # Close the progress bar window
 
@@ -94,13 +106,13 @@ def scan_dupe(folder, sub_f):
 
         match yes_or_no:
             case 'OK':
-                #threadingn to delete files faster 
+                #threadingn to delete files faster and so the main gui doesnt crash (don't respond)
                 thread1 = threading.Thread(target=send2trash, args=(dupe_files,))
                 thread1.start()
                 thread1.join()
                 sg.popup(f'{len(dupe_files)} files have been moved to the trash.')
             case _:
-                sg.popup_timed('0 files were deleted.')
+                sg.popup_timed('0 files have been moved to the trash.')
     else:
         sg.popup('There are no duplicate items in this folder')
 
